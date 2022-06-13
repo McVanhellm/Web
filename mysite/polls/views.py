94d695ae47ad6879663d9777782061abbec12f67
McVanhellm <mcvanhellm@gmail.com>
2022-06-13 14:36:55 +0300

    end

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,15 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Question
 # Create your views here.
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
